9e765ba962207fa80077b3e0522052fb143c6cd1
EucTech <euccreative@gmail.com>
2026-07-20 04:42:19 -0400

    feat: add language and goal options

---
+++ frontend/features/profile/data/options.ts
@@ -0,0 +1,29 @@
+export const SPOKEN_LANGUAGE_OPTIONS = [
+  "English",
+  "French",
+  "Arabic",
+  "Portuguese",
+  "Swahili",
+  "Yoruba",
+  "Amharic",
+  "Zulu",
+  "Hausa",
+  "Kinyarwanda",
+];
+
+export const TARGET_LANGUAGE_OPTIONS = [
+  "Swahili",
+  "Yoruba",
+  "Amharic",
+  "Zulu",
+  "Hausa",
+  "Kinyarwanda",
+];
+
+export const GOAL_OPTIONS = [
+  "Travel",
+  "Family & heritage",
+  "Career",
+  "Academic study",
+  "Culture & media",
+];
